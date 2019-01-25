3b201a476f0b0cf96387b924ed9c2e1d395d7ecf
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-01-25 21:54:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,8 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   else if array.length > 2
-    array.insert(-2, "and")
-    array.join(", ")
-  else if array.length ==1
-
-  end
+  ending = []
+  ending.push(array.slice!(-1))
 end
 end
 end
