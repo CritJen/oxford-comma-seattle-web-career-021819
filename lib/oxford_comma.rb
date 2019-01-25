910621f28861e73048feb4b49caf0af376ed0e5a
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-01-25 22:07:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   else if array.length > 2
-  ending = ["and "]
-  ending.push(array.slice!(-1))
-  return array.join(", ") + ending.join(" ")
+  array.join(, )
 end
 end
 end
