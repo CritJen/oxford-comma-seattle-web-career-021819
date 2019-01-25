7c04380c730d309443188c2a65497f42fb7c6d8b
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-01-25 22:23:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,12 +2,13 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   else if array.length > 2
+  ending = ["and "]
+  ending.push(array.slice!(-1))
   new_array = []
     array.each do |fruit|
       new_array.push(fruit + ", ")
     end
-    new_array.insert(-2 ,"and ")
-    return new_array.join("")
+  return new_array.join("") + ending.join("")
 end
 end
 end
